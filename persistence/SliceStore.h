0962ae41bf3726c2dfce27d29ce6537eb9d17f82
Larry Lindsey <llindsey@clm.utexas.edu>
2014-02-10 23:43:34 -0500

    SliceStore: simplified API. This should make it easier to write for DB's efficiently

--- persistence/SliceStore.h
+++ persistence/SliceStore.h
@@ -4,26 +4,13 @@
 #include <boost/shared_ptr.hpp>
 
 #include <sopnet/slices/Slice.h>
-#include <pipeline/all.h>
+#include <sopnet/slices/ConflictSets.h>
 
 #include <sopnet/slices/Slices.h>
-#include <sopnet/block/Block.h>
 #include <sopnet/block/Blocks.h>
-#include <sopnet/inference/LinearConstraints.h>
 
-class SliceStoreResult : public pipeline::Data
-{
-public:
-	SliceStoreResult() : count(0) {}
-	
-	SliceStoreResult(int c) : count(c)
-	{
-	}
-	
-	int count;
-	
-};
-
+#include <pipeline/Value.h>
+#include <pipeline/Data.h>
 
 /**
  * Abstract Data class that handles the practicalities of storing and retrieving Slices from a
@@ -34,51 +21,34 @@ class SliceStore : public pipeline::Data
 public:
     /**
      * Associates a slice with a block
-     * @param slice - the slice to store.
-     * @param block - the block containing the slice.
+     * @param slices - the slices to store.
+     * @param block - the block containing the slices.
      */
-    virtual void associate(const boost::shared_ptr<Slice>& slice,
-						   const boost::shared_ptr<Block>& block) = 0;
+    virtual void associate(const pipeline::Value<Slices> slices,
+						   const pipeline::Value<Block> block) = 0;
 
     /**
-     * Retrieve all slices that are at least partially contained in the given block.
-     * @param block - the Block for which to retrieve all slices.
+     * Retrieve all slices that are at least partially contained in the given blocks.
+     * @param blocks - the Blocks for which to retrieve all slices.
      */
-    virtual boost::shared_ptr<Slices> retrieveSlices(const boost::shared_ptr<Block>& block) = 0;
-
-	/**
-	 * Disassociate the given slice from the given block
-	 * @param slice - the slice to disassociate
-	 * @param block - the block to disassociate
-	 */
-	virtual void disassociate(const boost::shared_ptr<Slice>& slice,
-							  const boost::shared_ptr<Block>& block) = 0;
-
-	/**
-	 * Remove a Slice entirely from the store.
-	 * @param slice - the slice to remove.
-	 */
-	virtual void removeSlice(const boost::shared_ptr<Slice>& slice) = 0;
+    virtual pipeline::Value<Slices> retrieveSlices(const pipeline::Value<Blocks> blocks) = 0;
 
 	/**
 	 * Retrieve all Blocks associated with the given slice.
 	 * @param slice - the slice for which Blocks are to be retrieved.
 	 */
-	virtual boost::shared_ptr<Blocks> getAssociatedBlocks(
-		const boost::shared_ptr<Slice>& slice) = 0;
+	virtual pipeline::Value<Blocks> getAssociatedBlocks(
+		const pipeline::Value<Slice> slice) = 0;
 	
 	/**
-	 * Store a parent-child relationship between two slices.
+	 * Store a conflict set relationship. This requires the slices in question to already be
+	 * stored in this SliceStore.
+	 * @param conflictSets - the ConflictSets in question
 	 */
-	virtual void setParent(const boost::shared_ptr<Slice>& childSlice,
-						   const boost::shared_ptr<Slice>& parentSlice) = 0;
-
-	/**
-	 * Retrieve the children of the given Slice.
-	 */
-	virtual boost::shared_ptr<Slices> getChildren(const boost::shared_ptr<Slice>& parentSlice) = 0;
+	virtual void storeConflict(const pipeline::Value<ConflictSets> conflictSets) = 0;
 	
-	virtual boost::shared_ptr<Slice> getParent(const boost::shared_ptr<Slice>& childSlice) = 0;
+	virtual pipeline::Value<ConflictSets>
+		retrieveConflictSets(const pipeline::Value<Slices> slices) = 0;
 };
 
 #endif //SLICE_STORE_H__
